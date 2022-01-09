044fbd69d56b3bedd318be57ccfa824a2097dcf9
Daisy Nadauld <daisykunzler@gmail.com>
2022-01-08 22:00:42 -0700

    added instructions

--- main.py
+++ main.py
@@ -6,3 +6,7 @@
 '''This program will create a tic-tac-toe game for 2 users to play'''
 
 
+print("Welcome to the Tic-Tac-Toe game!")
+print("In this game, two players try to complete three x's or o's in a row, column, or diagonal.")
+print()
+print("To begin, select the number in the square you would like to mark.")
